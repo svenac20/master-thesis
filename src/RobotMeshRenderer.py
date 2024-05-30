c76c76d03f056c59342a244ad1658a57afb7d168
svenac20 <sven.scekic@gmail.com>
2024-05-30 17:56:30 +0200

    Move urdf and mesh files to separate folder. Add initial rednering

---
+++ src/RobotMeshRenderer.py
@@ -0,0 +1,85 @@
+from os.path import exists
+import os
+import torch
+import numpy as np
+import torch.nn as nn
+import torch.nn.functional as F
+
+# io utils
+from pytorch3d.io import load_obj
+
+# datastructures
+from pytorch3d.structures import Meshes
+
+# rendering components
+from pytorch3d.renderer import (
+	RasterizationSettings, MeshRenderer, MeshRasterizer, BlendParams,
+	SoftSilhouetteShader, HardPhongShader, PointLights, TexturesVertex,
+	PerspectiveCameras, Textures, look_at_view_transform, FoVPerspectiveCameras
+)
+
+
+class RobotMeshRenderer():
+	"""
+    Class that render robot mesh with differentiable renderer
+    """
+
+	def __init__(self, robot, mesh_files, device):
+
+		self.device = device
+		self.robot = robot
+		self.mesh_files = mesh_files
+		self.preload_verts = []
+		self.preload_faces = []
+
+		# preload the mesh to save loading time
+		for m_file in mesh_files:
+			assert exists(m_file)
+			preload_verts_i, preload_faces_idx_i, _ = load_obj(m_file)
+			preload_faces_i = preload_faces_idx_i.verts_idx
+			self.preload_verts.append(preload_verts_i)
+			self.preload_faces.append(preload_faces_i)
+
+	def get_robot_mesh(self, joint_angle):
+
+		R_list, t_list = self.robot.get_joint_RT(joint_angle)
+		assert len(self.mesh_files) == R_list.shape[0] and len(self.mesh_files) == t_list.shape[0]
+
+		verts_list = []
+		faces_list = []
+		verts_rgb_list = []
+		verts_count = 0
+		for i in range(len(self.mesh_files)):
+			verts_i = self.preload_verts[i]
+			faces_i = self.preload_faces[i]
+
+			R = torch.tensor(R_list[i], dtype=torch.float32)
+			t = torch.tensor(t_list[i], dtype=torch.float32)
+			verts_i = verts_i @ R.T + t
+			# verts_i = (R @ verts_i.T).T + t
+			faces_i = faces_i + verts_count
+
+			verts_count += verts_i.shape[0]
+
+			verts_list.append(verts_i.to(self.device))
+			faces_list.append(faces_i.to(self.device))
+
+			# Initialize each vertex to be white in color.
+			color = torch.rand(3)
+			verts_rgb_i = torch.ones_like(verts_i) * color  # (V, 3)
+			verts_rgb_list.append(verts_rgb_i.to(self.device))
+
+		verts = torch.concat(verts_list, dim=0)
+		faces = torch.concat(faces_list, dim=0)
+
+		verts_rgb = torch.concat(verts_rgb_list, dim=0)[None]
+		textures = Textures(verts_rgb=verts_rgb)
+
+		# Create a Meshes object
+		robot_mesh = Meshes(
+			verts=[verts.to(self.device)],
+			faces=[faces.to(self.device)],
+			textures=textures
+		)
+
+		return robot_mesh
--- src/main.py
+++ src/main.py
@@ -1,68 +1,32 @@
 import argparse
 import os
-import sys
 
-from src.robotMeshRenderer import RobotMeshRenderer
+import numpy as np
+import torch
+from pytorch3d.renderer import look_at_view_transform, FoVPerspectiveCameras, BlendParams, RasterizationSettings, \
+	PointLights, MeshRenderer, MeshRasterizer, SoftPhongShader
 
-base_dir = os.path.abspath(".")
-sys.path.append(base_dir)
+from PandaArm import PandaArm
+from Utils import image_grid
+from src.RobotMeshRenderer import RobotMeshRenderer
 
-from pathlib import Path
+base_dir = "/home/sscekic/master-thesis/src"
 
-
-def setup_robot_renderer(self, mesh_files):
-	# mesh_files: list of mesh files
-	focal_length = [-self.args.fx, -self.args.fy]
-	principal_point = [self.args.px, self.args.py]
-	image_size = [self.args.height, self.args.width]
-
-	robot_renderer = RobotMeshRenderer(
-		focal_length=focal_length, principal_point=principal_point, image_size=image_size,
-		robot=self.robot, mesh_files=mesh_files, device=self.device)
-
-	return robot_renderer
-
-
-def main(args):
-	mesh_files = [os.path.join(args.base_dir, "urdfs/meshes/collision/link0.obj"),
-				  os.path.join(args.base_dir, "urdfs/meshes/collision/link1.obj"),
-				  os.path.join(args.base_dir, "urdfs/meshes/collision/link2.obj"),
-				  os.path.join(args.base_dir, "urdfs/meshes/collision/link3.obj"),
-				  os.path.join(args.base_dir, "urdfs/meshes/collision/link4.obj"),
-				  os.path.join(args.base_dir, "urdfs/meshes/collision/link5.obj"),
-				  os.path.join(args.base_dir, "urdfs/meshes/collision/link6.obj"),
-				  os.path.join(args.base_dir, "urdfs/meshes/collision/link7.obj")]
-	robot_renderer = setup_robot_renderer(mesh_files)
-
-def get_args():
+if __name__ == '__main__':
 	parser = argparse.ArgumentParser()
 	args = parser.parse_args("")
-
-	args.data_folder = "/home/sscekic/master-thesis/src/panda-data"
-	args.base_dir = "/home/sscekic/master-thesis/src"
+	args.base_dir = "/home/sscekic/CtRNet-robot-pose-estimation"
 	args.use_gpu = False
-	args.trained_on_multi_gpus = False
-	args.keypoint_seg_model_path = os.path.join(args.base_dir, "weights/pretrain/baxter/net.pth")
-	# args.keypoint_seg_model_path = os.path.join(args.base_dir,"weights/baxter/net.pth")
-	args.urdf_file = os.path.join(args.base_dir, "urdfs/panda.urdf")
-
-	##### training parameters #####
-	## args.batch_size = 6
-	## args.num_workers = 6
-	## args.lr = 1e-6
-	## args.beta1 = 0.9
-	## args.n_epoch = 500
-	## args.out_dir = 'outputs/Baxter_arm/weights'
-	## args.ckp_per_epoch = 10
-	## args.reproj_err_scale = 1.0 / 100.0
-	################################
+	args.trained_on_multi_gpus = True
+	args.keypoint_seg_model_path = os.path.join(args.base_dir, "weights/panda/panda-3cam_azure/net.pth")
+	args.urdf_file = os.path.join(args.base_dir, "urdfs/Panda/panda.urdf")
 
 	args.robot_name = 'Panda'  # "Panda" or "Baxter_left_arm"
 	args.n_kp = 7
-	args.scale = 0.3125
-	args.height = 1536
-	args.width = 2048
-	args.fx, args.fy, args.px, args.py = 960.41357421875, 960.22314453125, 1021.7171020507812, 776.2381591796875
+	args.height = 480
+	args.width = 640
+	args.fx, args.fy, args.px, args.py = 399.6578776041667, 399.4959309895833, 319.8955891927083, 244.0602823893229
+	args.scale = 0.5  # scale the input image size to (320,240)
 
 	# scale the camera parameters
 	args.width = int(args.width * args.scale)
@@ -72,9 +36,65 @@ def get_args():
 	args.px = args.px * args.scale
 	args.py = args.py * args.scale
 
-	return args
+	mesh_files = [base_dir + "/urdfs/Panda/meshes/visual/link0/link0.obj",
+				  base_dir + "/urdfs/Panda/meshes/visual/link1/link1.obj",
+				  base_dir + "/urdfs/Panda/meshes/visual/link2/link2.obj",
+				  base_dir + "/urdfs/Panda/meshes/visual/link3/link3.obj",
+				  base_dir + "/urdfs/Panda/meshes/visual/link4/link4.obj",
+				  base_dir + "/urdfs/Panda/meshes/visual/link5/link5.obj",
+				  base_dir + "/urdfs/Panda/meshes/visual/link6/link6.obj",
+				  base_dir + "/urdfs/Panda/meshes/visual/link7/link7.obj",
+				  base_dir + "/urdfs/Panda/meshes/visual/hand/hand.obj",
+				  ]
 
+	device = 'cpu'
+	R, T = look_at_view_transform(2.8, 0, 50)
 
-if __name__ == '__main__':
-	args = get_args()
-	main(args)
+	cameras = FoVPerspectiveCameras(device=device, R=R, T=T, fov=30)
+	blend_params = BlendParams(sigma=1e-8, gamma=1e-8)
+	raster_settings = RasterizationSettings(
+		image_size=(args.width, args.height),
+		blur_radius=np.log(1. / 1e-4 - 1.) * blend_params.sigma,
+		faces_per_pixel=100,
+		max_faces_per_bin=100000,  # max_faces_per_bin=1000000,
+	)
+	lights = PointLights(device=device, location=((-2.0, -2.0, -2.0),))
+	renderer = MeshRenderer(
+		rasterizer=MeshRasterizer(
+			cameras=cameras,
+			raster_settings=raster_settings
+		),
+		shader=SoftPhongShader(device=device, cameras=cameras, lights=lights)
+	)
+
+	robot = PandaArm(args.urdf_file)
+	robot_renderer = RobotMeshRenderer(robot, mesh_files, device)
+	configuration = np.array([-5.32755313, -4.51632518, 1.02188406, 5.39148447, 1.54878585, 3.39261642, -6.11622803])
+	print(f"Currently rendering for configuration: {configuration}")
+	joint_angles = configuration
+	robot_mesh = robot_renderer.get_robot_mesh(joint_angles)
+
+	# Set batch size - this is the number of different viewpoints from which we want to render the mesh.
+	batch_size = 20
+
+	# Create a batch of meshes by repeating the cow mesh and associated textures.
+	# Meshes has a useful `extend` method which allows us do this very easily.
+	# This also extends the textures.
+	meshes = robot_mesh.extend(batch_size)
+
+	# Get a batch of viewing angles.
+	elev = torch.linspace(0, 180, batch_size)
+	azim = torch.linspace(-180, 180, batch_size)
+
+	# All the cameras helper methods support mixed type inputs and broadcasting. So we can
+	# view the camera from the same distance and specify dist=2.7 as a float,
+	# and then specify elevation and azimuth angles for each viewpoint as tensors.
+	R, T = look_at_view_transform(dist=2.7, elev=elev, azim=azim)
+	cameras = FoVPerspectiveCameras(device=device, R=R, T=T, fov=30)
+
+	# Move the light back in front of the cow which is facing the -z direction.
+	lights.location = torch.tensor([[0.0, 0.0, -3.0]], device=device)
+
+	images = renderer(meshes, cameras=cameras, lights=lights)
+	image_grid(images.cpu().numpy(), rows=4, cols=5, rgb=True)
+
